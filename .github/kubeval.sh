d76ebdfe4d619a16a1d03c48f77104d864ca67e9
Antonio Costa <a.costa@trikorasolutions.com>
2022-12-19 00:16:59 +0100

    chore: add debug info

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+echo "Starting..."
 set -euo pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- . | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 KUBEVAL_VERSION="v0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
-echo "Download kubeval..."
+echo "...download kubeval..."
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 echo "...extract kubeval..."
 tar -xf /tmp/kubeval.tar.gz kubeval
